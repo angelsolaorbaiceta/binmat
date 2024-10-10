9d6144239d496c74274682a22c4a36aadc28e8a8
angel <angelsolaorbaiceta@gmail.com>
2024-10-10 16:02:34 +0200

    Extract pattern from signature

---
+++ signature/match_test.go
@@ -0,0 +1,150 @@
+package signature
+
+import (
+	"testing"
+)
+
+func TestMatchPatternWithoutMask(t *testing.T) {
+	sig := makePattern([]byte{0x01, 0x02, 0x03})
+
+	t.Run("No enough bytes to match", func(t *testing.T) {
+		var (
+			data    = []byte{0x01, 0x02}
+			matches = sig.checkMatch(data)
+		)
+
+		if matches.len() != 0 {
+			t.Fatalf("expected 0 offsets, got %d", len(matches))
+		}
+	})
+
+	t.Run("One match starting at offset 0", func(t *testing.T) {
+		var (
+			data    = []byte{0x01, 0x02, 0x03, 0x04, 0x05}
+			matches = sig.checkMatch(data)
+		)
+
+		if matches.len() != 1 {
+			t.Fatalf("expected 1 offset, got %d", matches.len())
+		}
+		if matches[0] != 0 {
+			t.Fatalf("expected offset 0, got %d", matches[0])
+		}
+	})
+
+	t.Run("One match starting at offset 2", func(t *testing.T) {
+		var (
+			data    = []byte{0x00, 0x00, 0x01, 0x02, 0x03, 0x04, 0x05}
+			matches = sig.checkMatch(data)
+		)
+
+		if matches.len() != 1 {
+			t.Fatalf("expected 1 offset, got %d", matches.len())
+		}
+		if matches[0] != 2 {
+			t.Fatalf("expected offset 2, got %d", matches[0])
+		}
+	})
+
+	t.Run("No match", func(t *testing.T) {
+		var (
+			data    = []byte{0x01, 0x02, 0x04, 0x05, 0x06, 0x07}
+			matches = sig.checkMatch(data)
+		)
+
+		if matches.len() != 0 {
+			t.Fatalf("expected 0 offsets, got %d", matches.len())
+		}
+	})
+
+	t.Run("Multiple matches", func(t *testing.T) {
+		var (
+			data    = []byte{0x01, 0x02, 0x03, 0x04, 0x05, 0x01, 0x02, 0x03, 0x04, 0x05}
+			matches = sig.checkMatch(data)
+		)
+
+		if matches.len() != 2 {
+			t.Fatalf("expected 2 offsets, got %d", matches.len())
+		}
+		if matches[0] != 0 {
+			t.Fatalf("expected offset 0, got %d", matches[0])
+		}
+		if matches[1] != 5 {
+			t.Fatalf("expected offset 5, got %d", matches[1])
+		}
+	})
+}
+
+func TestMatchPatternWithMask(t *testing.T) {
+	sig := makePatternWithMask(
+		[]byte{0x01, 0x02, 0x03},
+		[]byte{MatchByte, AnyByte, MatchByte},
+	)
+
+	t.Run("No enough bytes to match", func(t *testing.T) {
+		var (
+			data    = []byte{0x01, 0x02}
+			matches = sig.checkMatch(data)
+		)
+
+		if matches.len() != 0 {
+			t.Fatalf("expected 0 offsets, got %d", matches.len())
+		}
+	})
+
+	t.Run("One match starting at offset 0", func(t *testing.T) {
+		var (
+			data    = []byte{0x01, 0xab, 0x03, 0x04, 0x05}
+			matches = sig.checkMatch(data)
+		)
+
+		if matches.len() != 1 {
+			t.Fatalf("expected 1 offset, got %d", matches.len())
+		}
+		if matches[0] != 0 {
+			t.Fatalf("expected offset 0, got %d", matches[0])
+		}
+	})
+
+	t.Run("One match starting at offset 2", func(t *testing.T) {
+		var (
+			data    = []byte{0x00, 0x00, 0x01, 0xab, 0x03, 0x04, 0x05}
+			matches = sig.checkMatch(data)
+		)
+
+		if matches.len() != 1 {
+			t.Fatalf("expected 1 offset, got %d", matches.len())
+		}
+		if matches[0] != 2 {
+			t.Fatalf("expected offset 2, got %d", matches[0])
+		}
+	})
+
+	t.Run("No match", func(t *testing.T) {
+		var (
+			data    = []byte{0x01, 0xab, 0x04, 0x05, 0x06, 0x07}
+			matches = sig.checkMatch(data)
+		)
+
+		if matches.len() != 0 {
+			t.Fatalf("expected 0 offsets, got %d", matches.len())
+		}
+	})
+
+	t.Run("Multiple matches", func(t *testing.T) {
+		var (
+			data    = []byte{0x01, 0xab, 0x03, 0x04, 0x05, 0x01, 0xcd, 0x03, 0x04, 0x05}
+			matches = sig.checkMatch(data)
+		)
+
+		if matches.len() != 2 {
+			t.Fatalf("expected 2 offsets, got %d", matches.len())
+		}
+		if matches[0] != 0 {
+			t.Fatalf("expected offset 0, got %d", matches[0])
+		}
+		if matches[1] != 5 {
+			t.Fatalf("expected offset 5, got %d", matches[1])
+		}
+	})
+}
---
+++ signature/pattern.go
@@ -0,0 +1,85 @@
+package signature
+
+type matchOffsets []int
+
+func (m matchOffsets) len() int {
+	return len(m)
+}
+
+type signaturePattern struct {
+	pattern       []byte
+	mask          []byte
+	maskedPattern []byte
+}
+
+// length returns the length of the pattern and mask.
+func (s *signaturePattern) length() int {
+	return len(s.pattern)
+}
+
+func makePattern(pattern []byte) *signaturePattern {
+	mask := make([]byte, len(pattern))
+	for i := range mask {
+		mask[i] = MatchByte
+	}
+
+	return makePatternWithMask(pattern, mask)
+}
+
+func makePatternWithMask(pattern, mask []byte) *signaturePattern {
+	if len(pattern) != len(mask) {
+		panic("pattern and mask length mismatch")
+	}
+
+	maskedPattern := make([]byte, len(pattern))
+	for i := range pattern {
+		maskedPattern[i] = pattern[i] & mask[i]
+	}
+
+	return &signaturePattern{
+		pattern:       pattern,
+		mask:          mask,
+		maskedPattern: maskedPattern,
+	}
+}
+
+// checkMatch reads the file from the byte slice and checks if the signature matches.
+// It returns all the offsets where the signature matches.
+//
+// The function expects the full file contents in a byte slice, as binaries themselves
+// are usually small enough to fit in memory.
+func (s *signaturePattern) checkMatch(data []byte) matchOffsets {
+	var (
+		offsets     []int
+		fileByte    byte
+		patternByte byte
+
+		patternFirstByte = s.pattern[0]
+		maskFirstByte    = s.mask[0]
+	)
+
+	for i := 0; i < len(data)-s.length(); i++ {
+		fileByte = data[i] & maskFirstByte
+
+		if fileByte != patternFirstByte {
+			continue
+		}
+
+		// The byte at i matches the first byte of the pattern.
+		// Check if the rest of the pattern matches.
+		for j := 1; j < s.length(); j++ {
+			fileByte = data[i+j] & s.mask[j]
+			patternByte = s.maskedPattern[j]
+
+			if patternByte != fileByte {
+				break
+			}
+
+			if j == s.length()-1 {
+				offsets = append(offsets, i)
+			}
+		}
+	}
+
+	return offsets
+}
